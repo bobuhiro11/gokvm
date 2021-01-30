69e3ebb7c44c59da0eb234d3034647375a46aff4
Nobuhiro MIKI <nob@bobuhiro11.net>
2021-01-30 16:15:54 +0900

    initial dev
    - add bzImage building
    - add initrd building
    - add travis-ci setting
    - add some implementation for KVM API
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # gokvm
+
+__This is work in progress project__.
+
+## Features
+
+- Cooperate with Linux KVM
+- Pure Golang (use only standard libraries)
+- Aimed to run only unmodified linux
+- No emuration of Networking, disk, etc.
+
+## How to use the CLI tool
+
+```bash
+$ go get github.com/nmi/gokvm
+$ gokvm -h
+```
+
+## How to use as golang package
+
+Pseudo code is as below:
+
+```go
+import "github.com/nmi/gokvm/kvm"
+
+ioportHandler := func (port uint32, isIn bool, value byte) {
+  ...
+}
+kvm.LinuxRun(bzImagePath, initrdPath, ioportHandler)
+```
+
+## Reference
+
+- [Using the KVM API, lwn.net](https://lwn.net/Articles/658511/)
+- [kvmtest.c, lwn.net](https://lwn.net/Articles/658512/)
+- [KVM tool](https://git.kernel.org/pub/scm/linux/kernel/git/will/kvmtool.git/about/)
+- [kvm-hello-world](https://github.com/dpw/kvm-hello-world)
+- [aghosn/kvm.go](https://gist.github.com/aghosn/f72c8e8f53bf99c3c4117f49677ab0b9)
+- [KVM HOST IN A FEW LINES OF CODE](https://zserge.com/posts/kvm/)
+- [zserge/kvm-host.c](https://gist.github.com/zserge/ae9098a75b2b83a1299d19b79b5fe488)
+- [CS 695: Virtualization and Cloud Computing, cse.iitb.ac.in](https://www.cse.iitb.ac.in/~cs695/)
+- [The Linux/x86 Boot Protocol, kernel.org](https://www.kernel.org/doc/html/latest/x86/boot.html)
+- [Build and run minimal Linux / Busybox systems in Qemu](https://gist.github.com/chrisdone/02e165a0004be33734ac2334f215380e)
+- [kvm_cost.go, google/gvisor](https://github.com/google/gvisor/blob/master/pkg/sentry/platform/kvm/kvm_const.go)
---
+++ kvm/kvm_test.go
@@ -0,0 +1,163 @@
+package kvm_test
+
+import (
+	"os"
+	"syscall"
+	"testing"
+	"unsafe"
+
+	"github.com/nmi/gokvm/kvm"
+)
+
+func TestGetAPIVersion(t *testing.T) {
+	t.Parallel()
+
+	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer devKVM.Close()
+
+	_, err = kvm.GetAPIVersion(devKVM.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreateVM(t *testing.T) {
+	t.Parallel()
+
+	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer devKVM.Close()
+
+	_, err = kvm.CreateVM(devKVM.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreateVCPU(t *testing.T) {
+	t.Parallel()
+
+	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer devKVM.Close()
+
+	vmFd, err := kvm.CreateVM(devKVM.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	vcpuFd, err := kvm.CreateVCPU(vmFd)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sregs, err := kvm.GetSregs(vcpuFd)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = kvm.SetSregs(vcpuFd, sregs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	regs, err := kvm.GetRegs(vcpuFd)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = kvm.SetRegs(vcpuFd, regs)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetVCPUMMapSize(t *testing.T) {
+	t.Parallel()
+
+	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer devKVM.Close()
+
+	_, err = kvm.GetVCPUMMmapSize(devKVM.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// mirror from https://lwn.net/Articles/658512/
+func TestAddNum(t *testing.T) {
+	t.Parallel()
+
+	devKVM, _ := os.OpenFile("/dev/kvm", os.O_RDWR, 0644)
+
+	defer devKVM.Close()
+
+	vmFd, _ := kvm.CreateVM(devKVM.Fd())
+	mem, _ := syscall.Mmap(-1, 0, 0x1000, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED|syscall.MAP_ANONYMOUS)
+
+	code := []byte{0xba, 0xf8, 0x03, 0x00, 0xd8, 0x04, '0', 0xee, 0xb0, '\n', 0xee, 0xf4}
+	for i := 0; i < len(code); i++ {
+		mem[i] = code[i]
+	}
+
+	_ = kvm.SetUserMemoryRegion(vmFd, &kvm.UserspaceMemoryRegion{
+		Slot:          0,
+		Flags:         0,
+		GuestPhysAddr: 0x1000,
+		MemorySize:    0x1000,
+		UserspaceAddr: uint64(uintptr(unsafe.Pointer(&mem[0]))),
+	})
+
+	vcpuFd, _ := kvm.CreateVCPU(vmFd)
+	mmapSize, _ := kvm.GetVCPUMMmapSize(devKVM.Fd())
+
+	r, _ := syscall.Mmap(int(vcpuFd), 0, int(mmapSize), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	run := (*kvm.RunData)(unsafe.Pointer(&r[0]))
+
+	sregs, _ := kvm.GetSregs(vcpuFd)
+	sregs.CS.Base, sregs.CS.Selector = 0, 0
+	_ = kvm.SetSregs(vcpuFd, sregs)
+	_ = kvm.SetRegs(vcpuFd, kvm.Regs{
+		RIP: 0x1000, RAX: 2, RBX: 2, RFLAGS: 0x2, RCX: 0, RDX: 0, RSI: 0,
+		RDI: 0, RSP: 0, RBP: 0, R8: 0, R9: 0, R10: 0, R11: 0, R12: 0,
+		R13: 0, R14: 0, R15: 0,
+	})
+
+	for {
+		_ = kvm.Run(vcpuFd)
+
+		switch run.ExitReason {
+		case kvm.EXITHLT:
+			return
+		case kvm.EXITIO:
+			direction, size, port, count, offset := run.IO()
+			if direction == kvm.EXITIOOUT && size == 1 && port == 0x3f8 && count == 1 {
+				p := uintptr(unsafe.Pointer(run))
+				c := *(*byte)(unsafe.Pointer(p + uintptr(offset)))
+				t.Logf("output from IO port: \"%c\"\n", c)
+
+				if c != '4' && c != '\n' {
+					t.Fatal("Unexpected Output")
+				}
+			} else {
+				t.Fatal("Unexpected KVM_EXIT_IO")
+			}
+		default:
+			t.Fatalf("Unexpected EXIT REASON = %d\n", run.ExitReason)
+		}
+	}
+}
