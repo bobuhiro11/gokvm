b4f08d89991d0a0957bb7442c1b0973834b16ce6
Nobuhiro MIKI <nob@bobuhiro11.net>
2021-02-25 23:21:54 +0900

    flag: disable graphical console as default
    refs: https://stackoverflow.com/questions/6718688/how-to-disable-graphical-console-after-kernel-boot
    
    $ ./gokvm . -k /boot/vmlinuz-5.8.9-101.fc31.x86_64 \
                -i /boot/initramfs-5.8.9-101.fc31.x86_64.img
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- flag/flag.go
+++ flag/flag.go
@@ -7,7 +7,7 @@ import (
 func ParseArgs(args []string) (string, string, string, error) {
 	kernel := flag.String("k", "./bzImage", "kernel image path")
 	initrd := flag.String("i", "./initrd", "initrd path")
-	params := flag.String("p", "console=ttyS0", "kernel command-line parameters")
+	params := flag.String("p", "console=ttyS0  vga=0", "kernel command-line parameters")
 
 	flag.Parse()
 
