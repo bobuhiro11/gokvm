3478e1511ebe84c2ed615c079cc8f0890f92f5ee
Nobuhiro MIKI <nob@bobuhiro11.net>
2021-11-05 08:36:03 +0000

    ebda: add intel multi processor config table header
    Now, we can proceed to the reading phase of the MP Config Table Header as shown below:
    
    [    0.031995][    T0] Intel MultiProcessor Specification v1.4
    [    0.033955][    T0]   mpc: 9fc40-9fc6a
    [    0.034585][    T0] MPTABLE: null local APIC address!
    [    0.035347][    T0] BIOS bug, MP table errors detected!...
    [    0.036177][    T0] ... disabling SMP support. (tell your hw vendor)
    [    0.037219][    T0] mapped APIC to ffffffffff5fd000 (        fee00000)
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- ebda/ebda.go
+++ ebda/ebda.go
@@ -3,6 +3,8 @@ package ebda
 import (
 	"bytes"
 	"encoding/binary"
+
+	"github.com/bobuhiro11/gokvm/bootparam"
 )
 
 // Extended BIOS Data Area (EBDA).
@@ -12,6 +14,7 @@ type EBDA struct {
 	// https://github.com/torvalds/linux/blob/2f111a6fd5b5297b4e92f53798ca086f7c7d33a4/arch/x86/kernel/mpparse.c#L597
 	_        [16 * 3]uint8
 	mpfIntel MPFIntel
+	mpcTable MPCTable
 }
 
 func (e *EBDA) Bytes() ([]byte, error) {
@@ -34,6 +37,13 @@ func New() (*EBDA, error) {
 
 	e.mpfIntel = *mpfIntel
 
+	mpcTable, err := NewMPCTable()
+	if err != nil {
+		return e, err
+	}
+
+	e.mpcTable = *mpcTable
+
 	return e, nil
 }
 
@@ -55,8 +65,9 @@ type MPFIntel struct {
 func NewMPFIntel() (*MPFIntel, error) {
 	m := &MPFIntel{}
 	m.Signature = (('_' << 24) | ('P' << 16) | ('M' << 8) | '_')
-	m.Length = 0 // this must be 1
+	m.Length = 1 // this must be 1
 	m.Specification = 4
+	m.PhysPtr = bootparam.EBDAStart + 0x40
 
 	var err error
 
@@ -86,6 +97,66 @@ func (m *MPFIntel) CalcCheckSum() (uint8, error) {
 }
 
 func (m *MPFIntel) Bytes() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	if err := binary.Write(buf, binary.LittleEndian, m); err != nil {
+		return []byte{}, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// MP Configuration Table Header
+// ported from https://github.com/torvalds/linux/blob/5bfc75d92/arch/x86/include/asm/mpspec_def.h#L37-L49
+type MPCTable struct {
+	Signature uint32
+	Length    uint16
+	Spec      uint8
+	CheckSum  uint8
+	OEM       [8]uint8
+	ProductID [12]uint8
+	OEMPtr    uint32
+	OEMSize   uint16
+	OEMCount  uint16
+	LAPIC     uint32 // Local APIC addresss must be set.
+	Reserved  uint32
+}
+
+func NewMPCTable() (*MPCTable, error) {
+	m := &MPCTable{}
+	m.Signature = (('P' << 24) | ('M' << 16) | ('C' << 8) | 'P')
+	m.Length = 44 // this field must contain the size of entries.
+	m.Spec = 1
+	m.LAPIC = 0xFEE00000
+
+	var err error
+
+	m.CheckSum, err = m.CalcCheckSum()
+	if err != nil {
+		return m, err
+	}
+
+	m.CheckSum ^= uint8(0xff)
+	m.CheckSum++
+
+	return m, nil
+}
+
+func (m *MPCTable) CalcCheckSum() (uint8, error) {
+	bytes, err := m.Bytes()
+	if err != nil {
+		return 0, err
+	}
+
+	tmp := uint32(0)
+	for _, b := range bytes {
+		tmp += uint32(b)
+	}
+
+	return uint8(tmp & 0xff), nil
+}
+
+func (m *MPCTable) Bytes() ([]byte, error) {
 	buf := new(bytes.Buffer)
 
 	if err := binary.Write(buf, binary.LittleEndian, m); err != nil {
