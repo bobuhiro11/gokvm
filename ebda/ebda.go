9959de6e669a121a3f66db534afe56238327efde
Nobuhiro MIKI <nob@bobuhiro11.net>
2021-11-12 01:57:14 +0000

    ebda: add data structures for MP_BUS, MP_INTSRC and MP_IOAPIC
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- ebda/ebda.go
+++ ebda/ebda.go
@@ -8,7 +8,8 @@ import (
 	"github.com/bobuhiro11/gokvm/bootparam"
 )
 
-// this must be fixed.
+// When we started working on the SMP implementation, we fixed it to 2 for
+// simplicity, and it should match the CLI arguments.
 const NumCPUs = 2
 
 // Extended BIOS Data Area (EBDA).
@@ -125,16 +126,37 @@ type MPCTable struct {
 	LAPIC     uint32 // Local APIC addresss must be set.
 	Reserved  uint32
 
-	mpcCPU [NumCPUs]MPCCpu
+	mpcCPU    [NumCPUs]MPCCpu
+	mpcBus    [2]MPCBus
+	mpcIntSrc [2]MPCIntSrc
+	mpcIOAPIC MPCIOAPIC
+}
+
+const (
+	IOAPICDefaultPhysBase = 0xfec00000
+	APICDefaultPhysBase   = 0xfee00000
+	APICBaseAddrStep      = 0x00400000
+	IOAPICBaseAddrStep    = 0x00100000
+	KVMAPICVersion        = 0x14
+	PCIBusID              = 0
+	ISABusID              = 1
+)
+
+func apicAddr(apic uint32) uint32 {
+	return APICDefaultPhysBase + apic*APICBaseAddrStep
+}
+
+func ioApicAddr(ioapic uint32) uint32 {
+	return IOAPICDefaultPhysBase + ioapic*IOAPICBaseAddrStep
 }
 
 func NewMPCTable() (*MPCTable, error) {
 	m := &MPCTable{}
 	m.Signature = (('P' << 24) | ('M' << 16) | ('C' << 8) | 'P')
 	m.Length = uint16(unsafe.Sizeof(MPCTable{})) // this field must contain the size of entries.
-	// m.Length += uint16(unsafe.Sizeof(MPCCpu{})) * NumCPUs
-	m.Spec = 1
-	m.LAPIC = 0xFEE00000
+	m.Spec = 4
+	m.LAPIC = apicAddr(0)
+	m.OEMCount = 7 // This must be the number of entries
 
 	var err error
 
@@ -146,6 +168,19 @@ func NewMPCTable() (*MPCTable, error) {
 
 		m.mpcCPU[i] = *mpcCPU
 	}
+
+	mpcBus, _ := NewMPCBus(true)
+	m.mpcBus[0] = *mpcBus
+	mpcBus, _ = NewMPCBus(false)
+	m.mpcBus[1] = *mpcBus
+
+	mpcIntSrc, _ := NewMPCIntSrc(true)
+	m.mpcIntSrc[0] = *mpcIntSrc
+	mpcIntSrc, _ = NewMPCIntSrc(false)
+	m.mpcIntSrc[1] = *mpcIntSrc
+
+	mpcIOAPIC, _ := NewMPCIOAPIC()
+	m.mpcIOAPIC = *mpcIOAPIC
 
 	m.CheckSum, err = m.CalcCheckSum()
 	if err != nil {
@@ -185,7 +220,7 @@ func (m *MPCTable) Bytes() ([]byte, error) {
 type MPCCpu struct {
 	Type        uint8
 	APICID      uint8 // Local APIC number
-	APICVER     uint8
+	APICVer     uint8
 	CPUFlag     uint8
 	CPUFeature  uint32
 	FeatureFlag uint32
@@ -197,12 +232,106 @@ func NewMPCCpu(i int) (*MPCCpu, error) {
 
 	m.Type = 0
 	m.APICID = uint8(i)
-	m.APICVER = 0x14
+	m.APICVer = KVMAPICVersion
 	m.CPUFlag |= 1 // enabled processor
 
 	if i == 0 {
 		m.CPUFlag |= 2 // boot processor
 	}
+	// refs: commit 0c7c14a747e9eb2c3cacef60fb74b0698c9d3adf in kvmtool
+	m.CPUFeature = 0x600  // STEPPING
+	m.FeatureFlag = 0x201 // CPU_FEATURE_APIC
+
+	return m, nil
+}
+
+type MPCBus struct {
+	Type    uint8
+	BusID   uint8
+	BusType [6]uint8
+}
+
+func NewMPCBus(isPCIBus bool) (*MPCBus, error) {
+	m := &MPCBus{}
+
+	m.Type = 1 // bus
+
+	if isPCIBus {
+		m.BusID = PCIBusID
+		m.BusType[0] = 'P'
+		m.BusType[1] = 'C'
+		m.BusType[2] = 'I'
+	} else {
+		m.BusID = ISABusID
+		m.BusType[0] = 'I'
+		m.BusType[1] = 'S'
+		m.BusType[2] = 'A'
+	}
+
+	m.BusType[3] = ' '
+	m.BusType[4] = ' '
+	m.BusType[5] = ' '
+
+	return m, nil
+}
+
+type MPCIntSrc struct {
+	Type      uint8
+	IrqType   uint8
+	IrqFlag   uint16
+	SrcBus    uint8
+	SrcBusIrq uint8
+	DstAPIC   uint8
+	DstIrq    uint8
+}
+
+const (
+	MPINT    = 0
+	MPNMI    = 1
+	MPSMI    = 2
+	MPExtINT = 3
+
+	MPIrqDirDefault = 0
+)
+
+func NewMPCIntSrc(isLINT0 bool) (*MPCIntSrc, error) {
+	m := &MPCIntSrc{}
+	m.Type = 4 // MP_LINTSRC
+
+	if isLINT0 {
+		m.IrqType = MPINT
+		m.IrqFlag = MPIrqDirDefault
+		m.SrcBus = ISABusID
+		m.SrcBusIrq = 0
+		m.DstAPIC = 0
+		m.DstIrq = 0
+	} else {
+		m.IrqType = MPNMI
+		m.IrqFlag = MPIrqDirDefault
+		m.SrcBus = ISABusID
+		m.SrcBusIrq = 0
+		m.DstAPIC = 0
+		m.DstIrq = 1
+	}
+
+	return m, nil
+}
+
+type MPCIOAPIC struct {
+	Type     uint8
+	APICID   uint8
+	APICVer  uint8
+	Flags    uint8
+	APICAddr uint32
+}
+
+func NewMPCIOAPIC() (*MPCIOAPIC, error) {
+	m := &MPCIOAPIC{}
+	m.Type = 2
+	m.APICID = 3 // nr_vcpu + 1
+	m.APICVer = KVMAPICVersion
+	m.Flags = 0x01 // MPC_APIC_USABLE 0x01
+	m.APICAddr = ioApicAddr(0)
 
 	return m, nil
 }
