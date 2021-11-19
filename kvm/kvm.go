92ef75655a2a288688f90cc386db4933b3019f4f
Nobuhiro MIKI <nob@bobuhiro11.net>
2021-11-19 05:53:57 +0000

    kvm: ignore EAGAIN & EINTR
    I did it! With the fixes I've made so far, I can now boot on multi-core.
    
    / # dmesg | grep -i smp:
    [    0.649012][    T1] smp: Bringing up secondary CPUs ...
    [    0.674250][    T1] smp: Brought up 1 node, 2 CPUs
    
    / # cat /proc/cpuinfo  | grep -i processor
    processor       : 0
    processor       : 1
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- kvm/kvm.go
+++ kvm/kvm.go
@@ -199,6 +199,12 @@ func CreateVCPU(vmFd uintptr, vcpuID int) (uintptr, error) {
 
 func Run(vcpuFd uintptr) error {
 	_, err := ioctl(vcpuFd, uintptr(kvmRun), uintptr(0))
+	if err != nil {
+		// refs: https://github.com/kvmtool/kvmtool/blob/415f92c33a227c02f6719d4594af6fad10f07abf/kvm-cpu.c#L44
+		if errors.Is(err, syscall.EAGAIN) || errors.Is(err, syscall.EINTR) {
+			return nil
+		}
+	}
 
 	return err
 }
