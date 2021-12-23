6a63e515d31b96f9eeaa2115d995a1a25f32b51f
Nobuhiro MIKI <nob@bobuhiro11.net>
2021-12-23 02:35:54 +0000

    pci: add struct for PCI Conf Space
    for example, virtio-pci device can be attached.
    
    / # lspci
    00:00.0 Non-VGA unclassified device: Intel Corporation Device 0000
    00:01.0 Non-VGA unclassified device: Red Hat, Inc. Virtio network device
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- pci/pci.go
+++ pci/pci.go
@@ -1,5 +1,10 @@
 package pci
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 // Configuration Space Access Mechanism #1
 //
 // refs
@@ -27,31 +32,97 @@ func (a address) isEnable() bool {
 	return ((uint32(a) >> 31) | 0x1) == 0x1
 }
 
+type deviceHeader struct {
+	vendorID   uint16
+	deviceID   uint16
+	_          uint16   // command
+	_          uint16   // status
+	_          uint8    // revisonID
+	_          [3]uint8 // classCode
+	_          uint8    // cacheLineSize
+	_          uint8    // latencyTimer
+	headerType uint8
+	_          uint8     // bist
+	_          [6]uint32 // baseAddressRegister
+	_          uint32    // cardbusCISPointer
+	_          uint16    // subsystemVendorID
+	_          uint16    // subsystemID
+	_          uint32    // expansionROMBaseAddress
+	_          uint8     // capabilitiesPointer
+	_          [7]uint8  // reserved
+	_          uint8     // interruptLine
+	_          uint8     // interruptPin
+	_          uint8     // minGnt
+	_          uint8     // maxLat
+}
+
+func (h *deviceHeader) Bytes() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	if err := binary.Write(buf, binary.LittleEndian, h); err != nil {
+		return []byte{}, err
+	}
+
+	return buf.Bytes(), nil
+}
+
 type PCI struct {
-	addr address
+	addr    address
+	headers []*deviceHeader
 }
 
 func New() *PCI {
-	return &PCI{
-		addr: 0xaabbccdd,
-	}
+	p := &PCI{}
+
+	// 00:00.0 for PCI bridge
+	p.headers = append(p.headers, &deviceHeader{
+		deviceID:   0x6000,
+		vendorID:   0x8086,
+		headerType: 1,
+	})
+
+	// 00:01.0 for Virtio PCI
+	p.headers = append(p.headers, &deviceHeader{
+		deviceID:   0x1000,
+		vendorID:   0x1AF4,
+		headerType: 0,
+	})
+
+	return p
 }
 
 func (p *PCI) PciConfDataIn(port uint64, values []byte) error {
 	// offset can be obtained from many source as below:
 	//        (address from IO port 0xcf8) & 0xfc + (IO port address for Data) - 0xCFC
 	// see pci_conf1_read in linux/arch/x86/pci/direct.c for more detail.
+	offset := int(p.addr.getRegisterOffset() + uint32(port-0xCFC))
 
-	offset := p.addr.getRegisterOffset() + uint32(port - 0xCFC)
-	if offset == 0x0a { // PCI_CLASS_DEVICE
-		values[0] = 0x00 // PCI_CLASS_BRIDGE_HOST
-		values[1] = 0x60
+	if !p.addr.isEnable() {
+		return nil
 	}
 
-	if offset == 0x00 { // PCI_VENDOR_ID
-		values[0] = 0x86 // PCI_VENDOR_ID_INTEL
-		values[1] = 0x80
+	if p.addr.getBusNumber() != 0 {
+		return nil
 	}
+
+	if p.addr.getFunctionNumber() != 0 {
+		return nil
+	}
+
+	slot := int(p.addr.getDeviceNumber())
+
+	if slot >= len(p.headers) {
+		return nil
+	}
+
+	b, err := p.headers[slot].Bytes()
+	if err != nil {
+		return err
+	}
+
+	l := len(values)
+	copy(values[:l], b[offset:offset+l])
+
 	return nil
 }
 
