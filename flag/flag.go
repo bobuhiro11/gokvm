82c0cf99b52275babc1414b773e4c93629c5b89b
Nobuhiro MIKI <nob@bobuhiro11.net>
2022-03-15 02:39:48 +0000

    add tsc_early_khz
    Because boot stops due to tsc calibration failure when running on travis-ci.
    
    [    0.012000][    T0] tsc: Unable to calibrate against PIT
    [    0.012000][    T0] tsc: No reference (HPET/PMTIMER) available
    [    0.012000][    T0] tsc: Marking TSC unstable due to could not calculate TSC khz
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- flag/flag.go
+++ flag/flag.go
@@ -12,7 +12,7 @@ func ParseArgs(args []string) (string, string, string, string, int, error) {
 
 	//  refs: commit 1621292e73770aabbc146e72036de5e26f901e86 in kvmtool
 	params := flag.String("p", `console=ttyS0 earlyprintk=serial noapic noacpi notsc `+
-		`debug apic=debug show_lapic=all mitigations=off lapic `+
+		`debug apic=debug show_lapic=all mitigations=off lapic tsc_early_khz=2000 `+
 		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" pci=realloc=off `+
 		`virtio_pci.force_legacy=1`, "kernel command-line parameters")
 
