aad7d07c3754fa6e6e876be91aea4df0753a4cb0
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-01-19 04:17:59 +0000

    add scripts/get_kernel.bash
    Accelerate CI by downloading pre-compiled kernel image from Internet.
    Only if there are changes in the kernel config, recompile locally.
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

---
+++ scripts/get_kernel.bash
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+# ----------------------------------------------------------------------
+# get_kernel.bash - download or build bzImage and vmlinux
+# ----------------------------------------------------------------------
+
+# Download pre-compiled bzImage
+curl -s -O -L -C - --retry 5 \
+  https://github.com/bobuhiro11/bins/raw/main/gokvm/bzImage
+
+# Download extract-ikconfig script
+curl -s -O -L -C - --retry 5 \
+  https://raw.githubusercontent.com/torvalds/linux/master/scripts/extract-ikconfig
+chmod a+x ./extract-ikconfig
+
+# Download extract-vmlinux script
+curl -s -O -L -C - --retry 5 \
+  https://raw.githubusercontent.com/torvalds/linux/master/scripts/extract-vmlinux
+chmod a+x ./extract-vmlinux
+
+# Check that bzImage was compiled with linux.config
+diff -u <(./extract-ikconfig bzImage) linux.config
+
+# If needed, we build bzImage in local
+if [ $? -ne 0 ]; then
+  version=$(awk '/Kernel Configuration/ {print $3}' ./linux.config)
+  major_version=$(echo $version | awk -F\. '{print $1}')
+  curl -s -O -L -C - --retry 5 \
+    https://cdn.kernel.org/pub/linux/kernel/v${major_version}.x/linux-${version}.tar.xz \
+    -o linux.tar.xz
+
+  tar Jxf ./linux.tar.xz --one-top-level=_linux --strip-components 1
+  cp linux.config _linux/.config
+  make -C _linux
+  cp _linux/arch/x86/boot/bzImage .
+fi
+
+# Extract vmlinux from bzImage
+./extract-vmlinux bzImage > vmlinux
