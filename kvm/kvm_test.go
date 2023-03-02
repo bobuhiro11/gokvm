bobuhiro11/gokvm

commit 1f44abb611a8e82222c9a9902159f4212935f4c8

Add kvm.GetMSRFeatureIndexList and related test
Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

kvm/msr.go
@@ -26,3 +26,19 @@ func GetMSRIndexList(kvmFd uintptr, list *MSRList) error {
 
 	return err
 }
+
+// GetMSRFeatureIndexList returns the list of MSRs that can be passed to the KVM_GET_MSRS system ioctl.
+// This lets userspace probe host capabilities and processor features that are exposed via MSRs
+// (e.g., VMX capabilities). This list also varies by kvm version and host processor, but does not change otherwise.
+func GetMSRFeatureIndexList(kvmFd uintptr, list *MSRList) error {
+	tmp := struct {
+		NMSRs uint32
+	}{
+		NMSRs: 100,
+	}
+	_, err := Ioctl(kvmFd,
+		IIOWR(kvmGetMSRFeatureIndexList, unsafe.Sizeof(tmp)),
+		uintptr(unsafe.Pointer(list)))
+
+	return err
+}

kvm/kvm_test.go
@@ -801,3 +801,46 @@ func TestGetMSRIndexList(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestGetMSRFeatureIndexList(t *testing.T) {
+	if os.Getuid() != 0 {
+		t.Skipf("Skipping test since we are not root")
+	}
+
+	t.Parallel()
+
+	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0o644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer devKVM.Close()
+
+	ret, err := kvm.CheckExtension(devKVM.Fd(), kvm.CapGETMSRFeatures)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if int(ret) <= 0 {
+		t.Skipf("Skipping test since CapGETMSRFeatures is disable")
+	}
+
+	list := kvm.MSRList{}
+	list.NMSRs = 100
+
+	if err = kvm.GetMSRFeatureIndexList(devKVM.Fd(), &list); err != nil {
+		t.Fatal(err)
+	}
+
+	var entryFound bool
+
+	for _, msr := range list.Indicies {
+		if msr != 0 {
+			entryFound = true
+		}
+	}
+
+	if !entryFound {
+		t.Log("no entry has been found and no error occurred. That's odd")
+	}
+}

kvm/kvm.go
@@ -14,7 +14,9 @@ const (
 	kvmGetVCPUMMapSize   = 0x04
 	kvmGetSupportedCPUID = 0x05
 
-	kvmGetEmulatedCPUID    = 0x09
+	kvmGetEmulatedCPUID       = 0x09
+	kvmGetMSRFeatureIndexList = 0x0A
+
 	kvmCreateVCPU          = 0x41
 	kvmGetDirtyLog         = 0x42
 	kvmSetNrMMUPages       = 0x44

commit e35962ac7221faea327426b8c8642b452ede4a9e

kvm: Add MSR probe in related unit tests.
The first time we get the number of MSRs, and the second time we get the
contents of the entries. This is the same mechanism as in the probe of
the test for the kernel [1].

[1]: https://github.com/torvalds/linux/blob/v5.16/tools/testing/selftests/kvm/x86_64/get_msr_index_features.c

Fixes: 1f44abb611a8 ("Add kvm.GetMSRFeatureIndexList and related test")
Fixes: 80552fcbb726 ("Add kvm.GetMSRIndexList and related test")
Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

kvm/msr.go
@@ -133,24 +133,22 @@ const (
 	MSRIA32VMXVMFUNC            MSR = 0x00000491
 )
 
+type MSRListBase struct {
+	NMSRs uint32
+}
+
 type MSRList struct {
-	NMSRs    uint32
-	Indicies [100]uint32
+	*MSRListBase
+	// Perhaps it could be generated dynamically,
+	// but if it is large enough, well it would work.
+	Indicies [1000]uint32
 }
 
 // GetMSRIndexList returns the guest msrs that are supported.
 // The list varies by kvm version and host processor, but does not change otherwise.
 func GetMSRIndexList(kvmFd uintptr, list *MSRList) error {
-	// This ugly hack is required to make the Ioctl work.
-	// If tried like kvm.GetSupportedCPUID it doesn't work.
-	// Maybe a difference in behavior on kernel side.
-	tmp := struct {
-		NMSRs uint32
-	}{
-		NMSRs: 100,
-	}
 	_, err := Ioctl(kvmFd,
-		IIOWR(kvmGetMSRIndexList, unsafe.Sizeof(tmp)),
+		IIOWR(kvmGetMSRIndexList, unsafe.Sizeof(MSRListBase{})),
 		uintptr(unsafe.Pointer(list)))
 
 	return err
@@ -160,13 +158,8 @@ func GetMSRIndexList(kvmFd uintptr, list *MSRList) error {
 // This lets userspace probe host capabilities and processor features that are exposed via MSRs
 // (e.g., VMX capabilities). This list also varies by kvm version and host processor, but does not change otherwise.
 func GetMSRFeatureIndexList(kvmFd uintptr, list *MSRList) error {
-	tmp := struct {
-		NMSRs uint32
-	}{
-		NMSRs: 100,
-	}
 	_, err := Ioctl(kvmFd,
-		IIOWR(kvmGetMSRFeatureIndexList, unsafe.Sizeof(tmp)),
+		IIOWR(kvmGetMSRFeatureIndexList, unsafe.Sizeof(MSRListBase{})),
 		uintptr(unsafe.Pointer(list)))
 
 	return err

kvm/kvm_test.go
@@ -795,8 +795,13 @@ func TestGetMSRIndexList(t *testing.T) {
 	}
 
 	list := kvm.MSRList{}
-	list.NMSRs = 100
 
+	// The first time we probe the number of MSRs.
+	if err := kvm.GetMSRIndexList(devKVM.Fd(), &list); !errors.Is(err, syscall.E2BIG) {
+		t.Fatal(err)
+	}
+
+	// The second time we get the contents of the entries.
 	if err := kvm.GetMSRIndexList(devKVM.Fd(), &list); err != nil {
 		t.Fatal(err)
 	}
@@ -826,9 +831,14 @@ func TestGetMSRFeatureIndexList(t *testing.T) {
 	}
 
 	list := kvm.MSRList{}
-	list.NMSRs = 100
 
-	if err = kvm.GetMSRFeatureIndexList(devKVM.Fd(), &list); err != nil {
+	// The first time we probe the number of MSRs.
+	if err := kvm.GetMSRFeatureIndexList(devKVM.Fd(), &list); !errors.Is(err, syscall.E2BIG) {
+		t.Fatal(err)
+	}
+
+	// The second time we get the contents of the entries.
+	if err := kvm.GetMSRFeatureIndexList(devKVM.Fd(), &list); err != nil {
 		t.Fatal(err)
 	}
 
@@ -841,7 +851,7 @@ func TestGetMSRFeatureIndexList(t *testing.T) {
 	}
 
 	if !entryFound {
-		t.Log("no entry has been found and no error occurred. That's odd")
+		t.Fatalf("no entry has been found and no error occurred. That's odd")
 	}
 }
 