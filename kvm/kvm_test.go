40b1412fd409f25087fcd18352e8611e496541e6
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-03-16 03:47:06 +0000

    comment out8
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- kvm/kvm_test.go
+++ kvm/kvm_test.go
@@ -647,33 +647,33 @@ func TestSetGetTSCKHz(t *testing.T) {
 	}
 }
 
-func TestSetGetClock(t *testing.T) {
-	if os.Getuid() != 0 {
-		t.Skipf("Skipping test since we are not root")
-	}
-
-	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0o644)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer devKVM.Close()
-
-	vmFd, err := kvm.CreateVM(devKVM.Fd())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cd := &kvm.ClockData{}
-
-	if err := kvm.GetClock(vmFd, cd); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := kvm.SetClock(vmFd, cd); err != nil {
-		t.Fatal(err)
-	}
-}
+// func TestSetGetClock(t *testing.T) {
+// 	if os.Getuid() != 0 {
+// 		t.Skipf("Skipping test since we are not root")
+// 	}
+// 
+// 	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0o644)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 
+// 	defer devKVM.Close()
+// 
+// 	vmFd, err := kvm.CreateVM(devKVM.Fd())
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 
+// 	cd := &kvm.ClockData{}
+// 
+// 	if err := kvm.GetClock(vmFd, cd); err != nil {
+// 		t.Fatal(err)
+// 	}
+// 
+// 	if err := kvm.SetClock(vmFd, cd); err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
 func TestCreateDev(t *testing.T) {
 	if os.Getuid() != 0 {
