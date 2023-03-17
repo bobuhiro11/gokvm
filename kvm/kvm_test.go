bobuhiro11/gokvm

commit 2b03e7ea4e5604cac421c1794e511ea3d9cff307

Add kvm.SetGSIRouting and related test
Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

kvm/irq.go
@@ -87,3 +87,31 @@ func SetPIT2(vmFd uintptr, pstate *PITState2) error {
 
 	return err
 }
+
+type IRQRoutingIRQChip struct {
+	IRQChip uint32
+	Pin     uint32
+}
+
+type IRQRoutingEntry struct {
+	GSI   uint32
+	Type  uint32
+	Flags uint32
+	_     uint32
+	IRQRoutingIRQChip
+}
+
+type IRQRouting struct {
+	Nr      uint32
+	Flags   uint32
+	Entries []IRQRoutingEntry
+}
+
+// SetGSIRouting sets the GSI routing table entries, overwriting any previously set entries.
+func SetGSIRouting(vmFd uintptr, irqR *IRQRouting) error {
+	_, err := Ioctl(vmFd,
+		IIOW(kvmSetGSIRouting, unsafe.Sizeof(irqR)),
+		uintptr(unsafe.Pointer(irqR)))
+
+	return err
+}

kvm/kvm_test.go
@@ -445,3 +445,37 @@ func TestGetSetPID2(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestSetGSIRouting(t *testing.T) {
+	if os.Getuid() != 0 {
+		t.Skipf("Skipping test since we are not root")
+	}
+
+	t.Parallel()
+
+	devKVM, err := os.OpenFile("/dev/kvm", os.O_RDWR, 0o644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer devKVM.Close()
+
+	vmFd, err := kvm.CreateVM(devKVM.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := kvm.CreateIRQChip(vmFd); err != nil {
+		t.Fatal(err)
+	}
+
+	irqR := &kvm.IRQRouting{
+		Nr:      0,
+		Flags:   0,
+		Entries: make([]kvm.IRQRoutingEntry, 1),
+	}
+
+	if err := kvm.SetGSIRouting(vmFd, irqR); err != nil {
+		t.Fatal(err)
+	}
+}

kvm/kvm.go
@@ -19,6 +19,7 @@ const (
 
 	kvmCreateIRQChip = 0x60
 	kvmIRQLineStatus = 0x67
+	kvmSetGSIRouting = 0x6A
 	kvmCreatePIT2    = 0x77
 
 	kvmRun      = 0x80

commit dcbb1b3aa7dd328ae1ebd3386e2e51339704f952

kvm: Add Bytes() to IRQRouting.
The memory area is not contiguous when Bytes() is not
implemented as shown below:

func TestIRQRouting (t *testing.T) {
  irqR := kvm.IRQRouting{
     Nr: 0x11223344,
     Flags: 0x55667788,
     Entries: []kvm.IRQRoutingEntry{
         {GSI: 0x99aabbcc},
     },
  }

  for i:= 0; i< 3; i++ {
     x := *(*uint32)(unsafe.Pointer((uintptr(unsafe.Pointer(&irqR)) + uintptr(4*i))))
     t.Logf("%x ", x)
  }
}

=== RUN   TestIRQRouting
    kvm_test.go:26: 11223344
    kvm_test.go:26: 55667788
    kvm_test.go:26: 6ff18

Fixes: 2b03e7ea4e56 ("Add kvm.SetGSIRouting and related test")
Closes: https://github.com/bobuhiro11/gokvm/issues/138
Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

kvm/irq.go
@@ -2,6 +2,10 @@
 package kvm
 
 import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
 	"unsafe"
 )
 
@@ -152,11 +156,61 @@ type IRQRouting struct {
 	Entries []IRQRoutingEntry
 }
 
+func (r *IRQRouting) Bytes() ([]byte, error) {
+	var buf bytes.Buffer
+
+	if err := binary.Write(&buf, binary.LittleEndian, r.Nr); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(&buf, binary.LittleEndian, r.Flags); err != nil {
+		return nil, err
+	}
+
+	for _, entry := range r.Entries {
+		if err := binary.Write(&buf, binary.LittleEndian, entry); err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
+}
+
+func NewIRQRouting(data []byte) (*IRQRouting, error) {
+	r := IRQRouting{}
+
+	var buf bytes.Buffer
+	if err := binary.Write(&buf, binary.LittleEndian, data); err != nil && !errors.Is(err, io.EOF) {
+		return nil, err
+	}
+
+	if err := binary.Read(&buf, binary.LittleEndian, &r.Nr); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(&buf, binary.LittleEndian, &r.Flags); err != nil {
+		return nil, err
+	}
+
+	r.Entries = make([]IRQRoutingEntry, r.Nr)
+
+	if err := binary.Read(&buf, binary.LittleEndian, &r.Entries); err != nil && !errors.Is(err, io.EOF) {
+		return nil, err
+	}
+
+	return &r, nil
+}
+
 // SetGSIRouting sets the GSI routing table entries, overwriting any previously set entries.
 func SetGSIRouting(vmFd uintptr, irqR *IRQRouting) error {
-	_, err := Ioctl(vmFd,
+	data, err := irqR.Bytes()
+	if err != nil {
+		return err
+	}
+
+	_, err = Ioctl(vmFd,
 		IIOW(kvmSetGSIRouting, unsafe.Sizeof(irqR)),
-		uintptr(unsafe.Pointer(irqR)))
+		uintptr(unsafe.Pointer(&data[0])))
 
 	return err
 }

kvm/kvm_test.go
@@ -12,6 +12,54 @@ import (
 	"github.com/bobuhiro11/gokvm/kvm"
 )
 
+func TestIRQRouting(t *testing.T) {
+	t.Parallel()
+
+	irqR := kvm.IRQRouting{
+		Nr:    0x00000002,
+		Flags: 0x55667788,
+		Entries: []kvm.IRQRoutingEntry{
+			{GSI: 0x99aabbcc},
+			{GSI: 0xdaedbeef},
+		},
+	}
+
+	data, err := irqR.Bytes()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want, got := uint32(0x00000002), *(*uint32)(unsafe.Pointer((uintptr(unsafe.Pointer(&data[0])) + uintptr(4*0))))
+	if want != got {
+		t.Fatalf("got: %d, want: %d", got, want)
+	}
+
+	want, got = uint32(0x55667788), *(*uint32)(unsafe.Pointer((uintptr(unsafe.Pointer(&data[0])) + uintptr(4*1))))
+	if want != got {
+		t.Fatalf("got: %d, want: %d", got, want)
+	}
+
+	want, got = uint32(0x99aabbcc), *(*uint32)(unsafe.Pointer((uintptr(unsafe.Pointer(&data[0])) + uintptr(4*2))))
+	if want != got {
+		t.Fatalf("got: %d, want: %d", got, want)
+	}
+
+	irqR2, err := kvm.NewIRQRouting(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want, got = uint32(0x99aabbcc), irqR2.Entries[0].GSI
+	if want != got {
+		t.Fatalf("got: %d, want: %d", got, want)
+	}
+
+	want, got = uint32(0xdaedbeef), irqR2.Entries[1].GSI
+	if want != got {
+		t.Fatalf("got: %d, want: %d", got, want)
+	}
+}
+
 func TestGetAPIVersion(t *testing.T) {
 	if os.Getuid() != 0 {
 		t.Skipf("Skipping test since we are not root")