bobuhiro11/gokvm

commit e69cd01336c31307acb1473f4f16ff10a94f8017

Replace flag handling with alecthomas/kong.
Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

flag/flag.go
@@ -1,26 +1,31 @@
 package flag
 
 import (
-	"flag"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
-// Config defines the configuration of the
-// virtual machine, as determined by flags.
-type Config struct {
-	Debug      bool
-	Dev        string
-	Kernel     string
-	Initrd     string
-	Params     string
-	TapIfName  string
-	Disk       string
-	NCPUs      int
-	MemSize    int
-	TraceCount int
+type context struct{}
+
+type cli struct {
+	Start StartCmd `cmd:"" help:"Starts a new VM"`
+	Debug DebugCmd `cmd:"" help:"Prints KVM capabilities"`
 }
+
+type StartCmd struct {
+	Kernel     string `flag:"" short:"k" name:"kernel" default:"./bzImage" help:"Path to linux kernel" type:"path"`
+	MemSize    string `flag:"" short:"m" name:"memsize" default:"1G" help:"memory size: as number[gGmM]"`
+	NCPUs      int    `flag:"" short:"c" name:"ncpus" default:"1" help:"Number of cpus"`
+	Dev        string `flag:"" short:"D" name:"kvmDevice" default:"/dev/kvm" help:"Path to Linux KVM device" type:"path"`
+	Initrd     string `flag:"" short:"i" name:"initrd" default:"./initrd" help:"Path to initrd" type:"path"`
+	Params     string `flag:"" short:"p" name:"params" help:"Linux kernel cmdline parameters"`
+	TapIfName  string `flag:"" short:"t" name:"tapifname" help:"name of tap interface" type:"path"`
+	Disk       string `flag:"" short:"d" name:"disk" help:"path of disk file" type:"path"`
+	TraceCount string `flag:"" short:"T" name:"traceCount" default:"0" help:"instructions to skip between trace prints"`
+}
+
+type DebugCmd struct{}
 
 // ParseSize parses a size string as number[gGmMkK]. The multiplier is optional,
 // and if not set, the unit passed in is used. The number can be any base and
@@ -52,44 +57,4 @@ func ParseSize(s, unit string) (int, error) {
 	}
 
 	return -1, fmt.Errorf("can not parse %q as num[gGmMkK]:%w", s, strconv.ErrSyntax)
-}
-
-// ParseArgs calls flag.Parse and a *Config or error.
-func ParseArgs(args []string) (*Config, error) {
-	c := &Config{}
-	flag.BoolVar(&c.Debug, "debug", false, "This flag provides a list of KVM capabilities and nothing more")
-	flag.StringVar(&c.Dev, "D", "/dev/kvm", "path of kvm device")
-	flag.StringVar(&c.Kernel, "k", "./bzImage", "kernel image path")
-	flag.StringVar(&c.Initrd, "i", "./initrd", "initrd path")
-	//  refs: commit 1621292e73770aabbc146e72036de5e26f901e86 in kvmtool
-	flag.StringVar(&c.Params, "p", `console=ttyS0 earlyprintk=serial noapic noacpi notsc `+
-		`debug apic=debug show_lapic=all mitigations=off lapic tsc_early_khz=2000 `+
-		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" pci=realloc=off `+
-		`virtio_pci.force_legacy=1 rdinit=/init init=/init `+
-		`gokvm.ipv4_addr=192.168.20.1/24`, "kernel command-line parameters")
-	flag.StringVar(&c.TapIfName, "t", "", `name of tap interface. `+
-		`If the string is an empty, no tap intarface is created. (default "")`)
-	flag.StringVar(&c.Disk, "d", "/dev/zero", "path of disk file (for /dev/vda)")
-
-	flag.IntVar(&c.NCPUs, "c", 1, "number of cpus")
-
-	msize := flag.String("m", "1G", "memory size: as number[gGmM], optional units, defaults to G")
-	tc := flag.String("T", "0", "how many instructions to skip between trace prints -- 0 means tracing disabled")
-
-	flag.Parse()
-
-	var err error
-	if err = flag.CommandLine.Parse(args[1:]); err != nil {
-		return nil, err
-	}
-
-	if c.MemSize, err = ParseSize(*msize, "g"); err != nil {
-		return nil, err
-	}
-
-	if c.TraceCount, err = ParseSize(*tc, ""); err != nil {
-		return nil, err
-	}
-
-	return c, nil
 }

flag/runs.go
@@ -1,0 +1,89 @@
+//go:build !test
+
+package flag
+
+import (
+	"log"
+
+	"github.com/alecthomas/kong"
+	"github.com/bobuhiro11/gokvm/probe"
+	"github.com/bobuhiro11/gokvm/vmm"
+)
+
+func Parse() error {
+	c := cli{}
+
+	programName := "gokvm"
+	programDesc := "gokvm is a small Linux KVM Hypervisor which supports kernel boot"
+
+	ctx := kong.Parse(&c,
+		kong.Name(programName),
+		kong.Description(programDesc),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+			Summary: true,
+		}))
+
+	err := ctx.Run(&context{})
+
+	return err
+}
+
+func (d *DebugCmd) Run(_ *context) error {
+	if err := probe.KVMCapabilities(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *StartCmd) Run(_ *context) error {
+	defparams := `console=ttyS0 earlyprintk=serial noapic noacpi notsc ` +
+		`debug apic=debug show_lapic=all mitigations=off lapic tsc_early_khz=2000 ` +
+		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" pci=realloc=off ` +
+		`virtio_pci.force_legacy=1 rdinit=/init init=/init ` +
+		`gokvm.ipv4_addr=192.168.20.1/24`
+
+	memSize, err := ParseSize(s.MemSize, "g")
+	if err != nil {
+		return err
+	}
+
+	traceC, err := ParseSize(s.TraceCount, "")
+	if err != nil {
+		return err
+	}
+
+	if len(s.Params) > 0 {
+		defparams = s.Params
+	}
+
+	c := &vmm.Config{
+		Dev:        s.Dev,
+		Kernel:     s.Kernel,
+		Initrd:     s.Initrd,
+		Params:     defparams,
+		TapIfName:  s.TapIfName,
+		Disk:       s.Disk,
+		NCPUs:      s.NCPUs,
+		MemSize:    memSize,
+		TraceCount: traceC,
+	}
+
+	vmm := vmm.New(*c)
+
+	if err := vmm.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := vmm.Setup(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := vmm.Boot(); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}

go.mod
@@ -2,4 +2,8 @@ module github.com/bobuhiro11/gokvm
 
 go 1.17
 
-require golang.org/x/arch v0.2.0
+require (
+	github.com/alecthomas/kong v0.8.0
+	golang.org/x/arch v0.2.0
+	golang.org/x/sys v0.11.0
+)

go.sum
@@ -1,3 +1,13 @@
+github.com/alecthomas/assert/v2 v2.1.0 h1:tbredtNcQnoSd3QBhQWI7QZ3XHOVkw1Moklp2ojoH/0=
+github.com/alecthomas/assert/v2 v2.1.0/go.mod h1:b/+1DI2Q6NckYi+3mXyH3wFb8qG37K/DuK80n7WefXA=
+github.com/alecthomas/kong v0.8.0 h1:ryDCzutfIqJPnNn0omnrgHLbAggDQM2VWHikE1xqK7s=
+github.com/alecthomas/kong v0.8.0/go.mod h1:n1iCIO2xS46oE8ZfYCNDqdR0b0wZNrXAIAqro/2132U=
+github.com/alecthomas/repr v0.1.0 h1:ENn2e1+J3k09gyj2shc0dHr/yjaWSHRlrJ4DPMevDqE=
+github.com/alecthomas/repr v0.1.0/go.mod h1:2kn6fqh/zIyPLmm3ugklbEi5hg5wS435eygvNfaDQL8=
+github.com/hexops/gotextdiff v1.0.3 h1:gitA9+qJrrTCsiCl7+kh75nPqQt1cx4ZkudSTLoUqJM=
+github.com/hexops/gotextdiff v1.0.3/go.mod h1:pSWU5MAI3yDq+fZBTazCSJysOMbxWL1BSow5/V2vxeg=
 golang.org/x/arch v0.2.0 h1:W1sUEHXiJTfjaFJ5SLo0N6lZn+0eO5gWD1MFeTGqQEY=
 golang.org/x/arch v0.2.0/go.mod h1:5om86z9Hs0C8fWVUuoMHwpExlXzs5Tkyp9hOrfG7pp8=
+golang.org/x/sys v0.11.0 h1:eG7RXZHdqOJ1i+0lgLgCpSXAp6M3LYlAo6osgSi0xOM=
+golang.org/x/sys v0.11.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 rsc.io/pdf v0.1.1/go.mod h1:n8OzWcQ6Sp37PL01nO98y4iUCRdTGarVfzxY20ICaU4=

main.go
@@ -1,40 +1,15 @@
+//go:build !test
+
 package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/bobuhiro11/gokvm/flag"
-	"github.com/bobuhiro11/gokvm/probe"
-	"github.com/bobuhiro11/gokvm/vmm"
 )
 
 func main() {
-	// This line break is required by golangci-lint but
-	// such breaks are considered an anti-pattern
-	// at Google.
-	c, err := flag.ParseArgs(os.Args)
-	if err != nil {
-		log.Fatalf("ParseArgs: %v", err)
-	}
-
-	if c.Debug {
-		if err := probe.KVMCapabilities(); err != nil {
-			log.Fatalf("kvm capabilities: %v", err)
-		}
-	}
-
-	vmm := vmm.New(*c)
-
-	if err := vmm.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := vmm.Setup(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := vmm.Boot(); err != nil {
+	if err := flag.Parse(); err != nil {
 		log.Fatal(err)
 	}
 }

flag/flag_test.go
@@ -2,81 +2,13 @@ package flag_test
 
 import (
 	"errors"
+	"os"
 	"strconv"
 	"testing"
 
+	"github.com/alecthomas/kong"
 	"github.com/bobuhiro11/gokvm/flag"
 )
-
-func TestParseArg(t *testing.T) {
-	t.Parallel()
-
-	args := []string{
-		"gokvm",
-		"-debug",
-		"-i",
-		"initrd_path",
-		"-k",
-		"kernel_path",
-		"-p",
-		"params",
-		"-t",
-		"tap_if_name",
-		"-c",
-		"2",
-		"-d",
-		"disk_path",
-		"-m",
-		"1G",
-		"-T",
-		"1M",
-	}
-
-	c, err := flag.ParseArgs(args)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !c.Debug {
-		t.Error("undesired debug value")
-	}
-
-	if c.Dev != "/dev/kvm" {
-		t.Error("invalid kvm  path")
-	}
-
-	if c.Kernel != "kernel_path" {
-		t.Error("invalid kernel image path")
-	}
-
-	if c.Initrd != "initrd_path" {
-		t.Error("invalid initrd path")
-	}
-
-	if c.Params != "params" {
-		t.Error("invalid kernel command-line parameters")
-	}
-
-	if c.TapIfName != "tap_if_name" {
-		t.Error("invalid name of tap interface")
-	}
-
-	if c.Disk != "disk_path" {
-		t.Errorf("invalid path of disk file: got %v, want %v", c.Disk, "disk_path")
-	}
-
-	if c.NCPUs != 2 {
-		t.Error("invalid number of vcpus")
-	}
-
-	if c.MemSize != 1<<30 {
-		t.Errorf("msize: got %#x, want %#x", c.MemSize, 1<<30)
-	}
-
-	if c.TraceCount != 1<<20 {
-		t.Errorf("trace: got %#x, want %#x", c.TraceCount, 1<<20)
-	}
-}
 
 func TestParsesize(t *testing.T) { // nolint:paralleltest
 	for _, tt := range []struct {
@@ -105,4 +37,40 @@ func TestParsesize(t *testing.T) { // nolint:paralleltest
 			t.Errorf("%s:parseMemSize(%s): got (%d, %v), want (%d, %v)", tt.name, tt.m, amt, err, tt.amt, tt.err)
 		}
 	}
+}
+
+func TestCmdlineParsing(t *testing.T) {
+	t.Parallel()
+
+	args := os.Args
+	defer func() {
+		os.Args = args
+	}()
+
+	var cli struct {
+		Start flag.StartCmd `cmd:"" help:"Starts a new VM"`
+	}
+
+	os.Args = []string{
+		"gokvm",
+		"start",
+		"-D",
+		"/dev/kvm",
+		"-k",
+		"kernel_path",
+		"-i",
+		"initrd_path",
+		"-m",
+		"1G",
+		"-c",
+		"2",
+		"-t",
+		"tap0",
+		"-d",
+		"/dev/null",
+		"-T",
+		"1",
+	}
+
+	kong.Parse(&cli, kong.Exit(func(_ int) { t.Fatal("parsing failed") }))
 }

Makefile
@@ -70,15 +70,15 @@ vmlinux: linux.config
 .PHONY: run
 run: initrd bzImage
 	$(MAKE) generate
-	go run . -c 4
+	go run . start -c 4
 
 .PHONY: run-system-kernel
 run-system-kernel:
 	$(MAKE) generate
 	# Implemented based on fedora's default path.
 	# Other distributions need to be considered.
-	go run . -p "console=ttyS0 pci=off earlyprintk=serial nokaslr rdinit=/bin/sh" \
-		-k $(shell ls -t /boot/vmlinuz*.x86_64 | head -n 1) \
+	go run . start -k $(shell ls -t /boot/vmlinuz*.x86_64 | head -n 1) \
+		-p "console=ttyS0 pci=off earlyprintk=serial nokaslr rdinit=/bin/sh" \
 		-i $(shell ls -t /boot/initramfs*.x86_64.img | head -n 1)
 
 .PHONY: generate
@@ -94,7 +94,7 @@ golangci: golangci-lint
 test: bzImage vda.img
 	$(MAKE) generate
 	$(MAKE) golangci
-	go test -coverprofile c.out ./...
+	go test -coverprofile c.out -tags test ./...
 
 .PHONY: clean
 clean:

term/term_test.go
@@ -1,9 +1,11 @@
 package term_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/bobuhiro11/gokvm/term"
+	"golang.org/x/sys/unix"
 )
 
 func TestIsTerminal(t *testing.T) {
@@ -11,5 +13,13 @@ func TestIsTerminal(t *testing.T) {
 
 	if term.IsTerminal() {
 		t.Fatalf("it is not terminal")
+	}
+}
+
+func TestSetRawMode(t *testing.T) {
+	t.Parallel()
+
+	if _, err := term.SetRawMode(); err != nil && !errors.Is(err, unix.ENOTTY) {
+		t.Fatalf("error SetRawMode: %v", err)
 	}
 }

commit 6449dd7737ffc618c63874fd521872f3c56ee024

Replace the parsing from 'kong' to standard package.
We believe that many people should be more familiar with
the standard package. Although gokvm has two subcommands,
it is sufficient for this project.

Prior to this commit, the 'flag' package had a dependency
on 'vmm' package. This is beyond the responsibility of parsing.

Fixes: e69cd01336c3 ("Replace flag handling with alecthomas/kong.")
Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

flag/flag.go
@@ -1,29 +1,104 @@
 package flag
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
-type CLI struct {
-	Boot  BootCMD  `cmd:"" help:"Boots a new VM"`
-	Probe ProbeCMD `cmd:"" help:"Probes KVM capabilities and prints them"`
+var ErrorInvalidSubcommands = errors.New("expected 'boot' or 'probe' subcommands")
+
+type BootArgs struct {
+	Kernel     string
+	MemSize    int
+	NCPUs      int
+	Dev        string
+	Initrd     string
+	Params     string
+	TapIfName  string
+	Disk       string
+	TraceCount int
 }
 
-type BootCMD struct {
-	Kernel     string `flag:"" short:"k" name:"kernel" default:"./bzImage" help:"Path to linux kernel" type:"path"`
-	MemSize    string `flag:"" short:"m" name:"memsize" default:"1G" help:"memory size: as number[gGmM]"`
-	NCPUs      int    `flag:"" short:"c" name:"ncpus" default:"1" help:"Number of cpus"`
-	Dev        string `flag:"" short:"D" name:"kvmDevice" default:"/dev/kvm" help:"Path to Linux KVM device" type:"path"`
-	Initrd     string `flag:"" short:"i" name:"initrd" help:"Path to initrd" type:"path"`
-	Params     string `flag:"" short:"p" name:"params" help:"Linux kernel cmdline parameters"`
-	TapIfName  string `flag:"" short:"t" name:"tapifname" help:"name of tap interface" type:"path"`
-	Disk       string `flag:"" short:"d" name:"disk" help:"path of disk file" type:"path"`
-	TraceCount string `flag:"" short:"T" name:"traceCount" default:"0" help:"instructions to skip between trace prints"`
+func parseBootArgs(args []string) (*BootArgs, error) {
+	bootCmd := flag.NewFlagSet("boot subcommand", flag.ExitOnError)
+	c := &BootArgs{}
+
+	bootCmd.StringVar(&c.Dev, "D", "/dev/kvm", "path of kvm device")
+	bootCmd.StringVar(&c.Kernel, "k", "./bzImage", "kernel image path")
+	bootCmd.StringVar(&c.Initrd, "i", "", "initrd path")
+	//  refs: commit 1621292e73770aabbc146e72036de5e26f901e86 in kvmtool
+	bootCmd.StringVar(&c.Params, "p", `console=ttyS0 earlyprintk=serial `+
+		`noapic noacpi notsc nowatchdog `+
+		`nmi_watchdog=0 debug apic=debug show_lapic=all mitigations=off `+
+		`lapic tsc_early_khz=2000 `+
+		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" `+
+		`pci=realloc=off `+
+		`virtio_pci.force_legacy=1 rdinit=/init init=/init `+
+		`gokvm.ipv4_addr=192.168.20.1/24`,
+		"kernel command-line parameters")
+	bootCmd.StringVar(&c.TapIfName, "t", "", `name of tap interface. `+
+		`If the string is an empty, no tap intarface is created. (default"")`)
+	bootCmd.StringVar(&c.Disk, "d", "", "path of disk file (for /dev/vda)")
+
+	bootCmd.IntVar(&c.NCPUs, "c", 1, "number of cpus")
+
+	msize := bootCmd.String("m", "1G",
+		"memory size: as number[gGmM], optional units, defaults to G")
+	tc := bootCmd.String("T", "0",
+		"how many instructions to skip between trace prints -- 0 means tracing disabled")
+
+	var err error
+
+	if err = bootCmd.Parse(args); err != nil {
+		return nil, err
+	}
+
+	if c.MemSize, err = ParseSize(*msize, "g"); err != nil {
+		return nil, err
+	}
+
+	if c.TraceCount, err = ParseSize(*tc, ""); err != nil {
+		return nil, err
+	}
+
+	return c, nil
 }
 
-type ProbeCMD struct{}
+type ProbeArgs struct{}
+
+func parseProbeArgs(args []string) (*ProbeArgs, error) {
+	probeCmd := flag.NewFlagSet("probe subcommand", flag.ExitOnError)
+	c := &ProbeArgs{}
+
+	if err := probeCmd.Parse(args); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func ParseArgs(args []string) (*BootArgs, *ProbeArgs, error) {
+	if len(args) < 2 {
+		return nil, nil, ErrorInvalidSubcommands
+	}
+
+	switch args[1] {
+	case "boot":
+		conf, err := parseBootArgs(args[2:])
+
+		return conf, nil, err
+
+	case "probe":
+		conf, err := parseProbeArgs(args[2:])
+
+		return nil, conf, err
+	}
+
+	return nil, nil, ErrorInvalidSubcommands
+}
 
 // ParseSize parses a size string as number[gGmMkK]. The multiplier is optional,
 // and if not set, the unit passed in is used. The number can be any base and

flag/runs.go
@@ -1,85 +1,0 @@
-package flag
-
-import (
-	"log"
-
-	"github.com/alecthomas/kong"
-	"github.com/bobuhiro11/gokvm/probe"
-	"github.com/bobuhiro11/gokvm/vmm"
-)
-
-func Parse() *kong.Context {
-	c := CLI{}
-
-	programName := "gokvm"
-	programDesc := "gokvm is a small Linux KVM Hypervisor which supports kernel boot"
-
-	ctx := kong.Parse(&c,
-		kong.Name(programName),
-		kong.Description(programDesc),
-		kong.UsageOnError(),
-		kong.ConfigureHelp(kong.HelpOptions{
-			Compact: true,
-			Summary: true,
-		}))
-
-	return ctx
-}
-
-func (d *ProbeCMD) Run() error {
-	if err := probe.KVMCapabilities(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s *BootCMD) Run() error {
-	defparams := `console=ttyS0 earlyprintk=serial noapic noacpi notsc nowatchdog ` +
-		`nmi_watchdog=0 debug apic=debug show_lapic=all mitigations=off lapic tsc_early_khz=2000 ` +
-		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" pci=realloc=off ` +
-		`virtio_pci.force_legacy=1 rdinit=/init init=/init ` +
-		`gokvm.ipv4_addr=192.168.20.1/24`
-
-	memSize, err := ParseSize(s.MemSize, "g")
-	if err != nil {
-		return err
-	}
-
-	traceC, err := ParseSize(s.TraceCount, "")
-	if err != nil {
-		return err
-	}
-
-	if len(s.Params) > 0 {
-		defparams = s.Params
-	}
-
-	c := &vmm.Config{
-		Dev:        s.Dev,
-		Kernel:     s.Kernel,
-		Initrd:     s.Initrd,
-		Params:     defparams,
-		TapIfName:  s.TapIfName,
-		Disk:       s.Disk,
-		NCPUs:      s.NCPUs,
-		MemSize:    memSize,
-		TraceCount: traceC,
-	}
-
-	vmm := vmm.New(*c)
-
-	if err := vmm.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := vmm.Setup(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := vmm.Boot(); err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
-}

go.mod
@@ -3,7 +3,6 @@ module github.com/bobuhiro11/gokvm
 go 1.20
 
 require (
-	github.com/alecthomas/kong v0.8.0
 	golang.org/x/arch v0.2.0
 	golang.org/x/sys v0.11.0
 )

go.sum
@@ -1,8 +1,3 @@
-github.com/alecthomas/assert/v2 v2.1.0 h1:tbredtNcQnoSd3QBhQWI7QZ3XHOVkw1Moklp2ojoH/0=
-github.com/alecthomas/kong v0.8.0 h1:ryDCzutfIqJPnNn0omnrgHLbAggDQM2VWHikE1xqK7s=
-github.com/alecthomas/kong v0.8.0/go.mod h1:n1iCIO2xS46oE8ZfYCNDqdR0b0wZNrXAIAqro/2132U=
-github.com/alecthomas/repr v0.1.0 h1:ENn2e1+J3k09gyj2shc0dHr/yjaWSHRlrJ4DPMevDqE=
-github.com/hexops/gotextdiff v1.0.3 h1:gitA9+qJrrTCsiCl7+kh75nPqQt1cx4ZkudSTLoUqJM=
 golang.org/x/arch v0.2.0 h1:W1sUEHXiJTfjaFJ5SLo0N6lZn+0eO5gWD1MFeTGqQEY=
 golang.org/x/arch v0.2.0/go.mod h1:5om86z9Hs0C8fWVUuoMHwpExlXzs5Tkyp9hOrfG7pp8=
 golang.org/x/sys v0.11.0 h1:eG7RXZHdqOJ1i+0lgLgCpSXAp6M3LYlAo6osgSi0xOM=

main.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/bobuhiro11/gokvm/flag"
+	"github.com/bobuhiro11/gokvm/probe"
+	"github.com/bobuhiro11/gokvm/vmm"
 )
 
 func main() {
-	if err := flag.Parse().Run(); err != nil {
+	bootArgs, probeArgs, err := flag.ParseArgs(os.Args)
+	if err != nil {
 		log.Fatal(err)
+	}
+
+	if bootArgs != nil {
+		c := &vmm.Config{
+			Dev:        bootArgs.Dev,
+			Kernel:     bootArgs.Kernel,
+			Initrd:     bootArgs.Initrd,
+			Params:     bootArgs.Params,
+			TapIfName:  bootArgs.TapIfName,
+			Disk:       bootArgs.Disk,
+			NCPUs:      bootArgs.NCPUs,
+			MemSize:    bootArgs.MemSize,
+			TraceCount: bootArgs.TraceCount,
+		}
+
+		vmm := vmm.New(*c)
+
+		if err := vmm.Init(); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := vmm.Setup(); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := vmm.Boot(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if probeArgs != nil {
+		if err := probe.KVMCapabilities(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }

flag/flag_test.go
@@ -2,7 +2,6 @@ package flag_test
 
 import (
 	"errors"
-	"os"
 	"strconv"
 	"testing"
 
@@ -38,48 +37,143 @@ func TestParsesize(t *testing.T) { // nolint:paralleltest
 	}
 }
 
-func TestCmdlineBootParsing(t *testing.T) { // nolint:paralleltest
-	// Omit t.Parallel() because it modifies os.Args.
-	args := os.Args
-	defer func() {
-		os.Args = args
-	}()
+func TestParseBootArgs(t *testing.T) {
+	t.Parallel()
 
-	os.Args = []string{
+	args := []string{
 		"gokvm",
 		"boot",
-		"-D",
-		"/dev/kvm",
-		"-k",
-		"kernel_path",
 		"-i",
 		"initrd_path",
-		"-m",
-		"1G",
+		"-k",
+		"kernel_path",
+		"-p",
+		"params",
+		"-t",
+		"tap_if_name",
 		"-c",
 		"2",
-		"-t",
-		"tap0",
 		"-d",
-		"/dev/null",
+		"disk_path",
+		"-m",
+		"1G",
 		"-T",
-		"1",
+		"1M",
 	}
 
-	flag.Parse()
+	c, _, err := flag.ParseArgs(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c.Dev != "/dev/kvm" {
+		t.Error("invalid kvm  path")
+	}
+
+	if c.Kernel != "kernel_path" {
+		t.Error("invalid kernel image path")
+	}
+
+	if c.Initrd != "initrd_path" {
+		t.Error("invalid initrd path")
+	}
+
+	if c.Params != "params" {
+		t.Error("invalid kernel command-line parameters")
+	}
+
+	if c.TapIfName != "tap_if_name" {
+		t.Error("invalid name of tap interface")
+	}
+
+	if c.Disk != "disk_path" {
+		t.Errorf("invalid path of disk file: got %v, want %v", c.Disk, "disk_path")
+	}
+
+	if c.NCPUs != 2 {
+		t.Error("invalid number of vcpus")
+	}
+
+	if c.MemSize != 1<<30 {
+		t.Errorf("msize: got %#x, want %#x", c.MemSize, 1<<30)
+	}
+
+	if c.TraceCount != 1<<20 {
+		t.Errorf("trace: got %#x, want %#x", c.TraceCount, 1<<20)
+	}
 }
 
-func TestCmdlineProbeParsing(t *testing.T) { // nolint:paralleltest
-	// Omit t.Parallel() because it modifies os.Args.
-	args := os.Args
-	defer func() {
-		os.Args = args
-	}()
+func TestParseBootArgsWithDefaults(t *testing.T) {
+	t.Parallel()
 
-	os.Args = []string{
+	args := []string{
+		"gokvm",
+		"boot",
+	}
+
+	c, _, err := flag.ParseArgs(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c.Dev != "/dev/kvm" {
+		t.Error("invalid kvm path")
+	}
+
+	if c.Kernel != "./bzImage" {
+		t.Error("invalid kernel image path")
+	}
+
+	if c.Initrd != "" {
+		t.Error("invalid initrd path")
+	}
+
+	if c.Params != `console=ttyS0 earlyprintk=serial `+
+		`noapic noacpi notsc nowatchdog `+
+		`nmi_watchdog=0 debug apic=debug show_lapic=all mitigations=off `+
+		`lapic tsc_early_khz=2000 `+
+		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" `+
+		`pci=realloc=off `+
+		`virtio_pci.force_legacy=1 rdinit=/init init=/init `+
+		`gokvm.ipv4_addr=192.168.20.1/24` {
+		t.Error("invalid kernel command-line parameters")
+	}
+
+	if c.TapIfName != "" {
+		t.Error("invalid name of tap interface")
+	}
+
+	if c.Disk != "" {
+		t.Errorf("invalid path of disk file: got %v, want %v", c.Disk, "disk_path")
+	}
+
+	if c.NCPUs != 1 {
+		t.Error("invalid number of vcpus")
+	}
+
+	if c.MemSize != 1<<30 {
+		t.Errorf("msize: got %#x, want %#x", c.MemSize, 1<<30)
+	}
+
+	if c.TraceCount != 0 {
+		t.Errorf("trace: got %#x, want %#x", c.TraceCount, 1<<20)
+	}
+}
+
+func TestParseProbeArgs(t *testing.T) {
+	t.Parallel()
+
+	args := []string{
 		"gokvm",
 		"probe",
 	}
 
-	flag.Parse()
+	_, probeConfig, err := flag.ParseArgs(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if probeConfig == nil {
+		t.Fatal("probeConfig is nil")
+	}
 }
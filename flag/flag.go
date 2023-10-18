6449dd7737ffc618c63874fd521872f3c56ee024
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-10-18 09:04:49 +0000

    Replace the parsing from 'kong' to standard package.
    We believe that many people should be more familiar with
    the standard package. Although gokvm has two subcommands,
    it is sufficient for this project.
    
    Prior to this commit, the 'flag' package had a dependency
    on 'vmm' package. This is beyond the responsibility of parsing.
    
    Fixes: e69cd01336c3 ("Replace flag handling with alecthomas/kong.")
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- flag/flag.go
+++ flag/flag.go
@@ -1,29 +1,104 @@
 package flag
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
-type CLI struct {
-	Boot  BootCMD  `cmd:"" help:"Boots a new VM"`
-	Probe ProbeCMD `cmd:"" help:"Probes KVM capabilities and prints them"`
+var ErrorInvalidSubcommands = errors.New("expected 'boot' or 'probe' subcommands")
+
+type BootArgs struct {
+	Kernel     string
+	MemSize    int
+	NCPUs      int
+	Dev        string
+	Initrd     string
+	Params     string
+	TapIfName  string
+	Disk       string
+	TraceCount int
 }
 
-type BootCMD struct {
-	Kernel     string `flag:"" short:"k" name:"kernel" default:"./bzImage" help:"Path to linux kernel" type:"path"`
-	MemSize    string `flag:"" short:"m" name:"memsize" default:"1G" help:"memory size: as number[gGmM]"`
-	NCPUs      int    `flag:"" short:"c" name:"ncpus" default:"1" help:"Number of cpus"`
-	Dev        string `flag:"" short:"D" name:"kvmDevice" default:"/dev/kvm" help:"Path to Linux KVM device" type:"path"`
-	Initrd     string `flag:"" short:"i" name:"initrd" help:"Path to initrd" type:"path"`
-	Params     string `flag:"" short:"p" name:"params" help:"Linux kernel cmdline parameters"`
-	TapIfName  string `flag:"" short:"t" name:"tapifname" help:"name of tap interface" type:"path"`
-	Disk       string `flag:"" short:"d" name:"disk" help:"path of disk file" type:"path"`
-	TraceCount string `flag:"" short:"T" name:"traceCount" default:"0" help:"instructions to skip between trace prints"`
+func parseBootArgs(args []string) (*BootArgs, error) {
+	bootCmd := flag.NewFlagSet("boot subcommand", flag.ExitOnError)
+	c := &BootArgs{}
+
+	bootCmd.StringVar(&c.Dev, "D", "/dev/kvm", "path of kvm device")
+	bootCmd.StringVar(&c.Kernel, "k", "./bzImage", "kernel image path")
+	bootCmd.StringVar(&c.Initrd, "i", "", "initrd path")
+	//  refs: commit 1621292e73770aabbc146e72036de5e26f901e86 in kvmtool
+	bootCmd.StringVar(&c.Params, "p", `console=ttyS0 earlyprintk=serial `+
+		`noapic noacpi notsc nowatchdog `+
+		`nmi_watchdog=0 debug apic=debug show_lapic=all mitigations=off `+
+		`lapic tsc_early_khz=2000 `+
+		`dyndbg="file arch/x86/kernel/smpboot.c +plf ; file drivers/net/virtio_net.c +plf" `+
+		`pci=realloc=off `+
+		`virtio_pci.force_legacy=1 rdinit=/init init=/init `+
+		`gokvm.ipv4_addr=192.168.20.1/24`,
+		"kernel command-line parameters")
+	bootCmd.StringVar(&c.TapIfName, "t", "", `name of tap interface. `+
+		`If the string is an empty, no tap intarface is created. (default"")`)
+	bootCmd.StringVar(&c.Disk, "d", "", "path of disk file (for /dev/vda)")
+
+	bootCmd.IntVar(&c.NCPUs, "c", 1, "number of cpus")
+
+	msize := bootCmd.String("m", "1G",
+		"memory size: as number[gGmM], optional units, defaults to G")
+	tc := bootCmd.String("T", "0",
+		"how many instructions to skip between trace prints -- 0 means tracing disabled")
+
+	var err error
+
+	if err = bootCmd.Parse(args); err != nil {
+		return nil, err
+	}
+
+	if c.MemSize, err = ParseSize(*msize, "g"); err != nil {
+		return nil, err
+	}
+
+	if c.TraceCount, err = ParseSize(*tc, ""); err != nil {
+		return nil, err
+	}
+
+	return c, nil
 }
 
-type ProbeCMD struct{}
+type ProbeArgs struct{}
+
+func parseProbeArgs(args []string) (*ProbeArgs, error) {
+	probeCmd := flag.NewFlagSet("probe subcommand", flag.ExitOnError)
+	c := &ProbeArgs{}
+
+	if err := probeCmd.Parse(args); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func ParseArgs(args []string) (*BootArgs, *ProbeArgs, error) {
+	if len(args) < 2 {
+		return nil, nil, ErrorInvalidSubcommands
+	}
+
+	switch args[1] {
+	case "boot":
+		conf, err := parseBootArgs(args[2:])
+
+		return conf, nil, err
+
+	case "probe":
+		conf, err := parseProbeArgs(args[2:])
+
+		return nil, conf, err
+	}
+
+	return nil, nil, ErrorInvalidSubcommands
+}
 
 // ParseSize parses a size string as number[gGmMkK]. The multiplier is optional,
 // and if not set, the unit passed in is used. The number can be any base and
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/bobuhiro11/gokvm
 go 1.20
 
 require (
-	github.com/alecthomas/kong v0.8.0
 	golang.org/x/arch v0.2.0
 	golang.org/x/sys v0.11.0
 )
